85e2f86338cae0ffdb81bf5b6b060072cabcb958
glazlk <andre749539@gmail.com>
2026-05-18 08:07:11 +0300

    refactor: replace star icon with trophy for rating display

--- apps/tma/src/components/icons/StarIcon.tsx
+++ apps/tma/src/components/icons/StarIcon.tsx
@@ -1,9 +1,9 @@
-import { Star } from '@phosphor-icons/react';
+import { Trophy } from '@phosphor-icons/react';
 import type { IconProps } from '@phosphor-icons/react';
 
 export function StarIcon({ className = '', size = 24, ...rest }: Partial<IconProps>) {
   return (
-    <Star
+    <Trophy
       weight="fill"
       size={size}
       className={`drop-shadow-[0_2px_3px_rgba(0,0,0,0.4)] ${className}`}
